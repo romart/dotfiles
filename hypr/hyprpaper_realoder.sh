bbe2ac402d7c0de28d48377a8fb410e8551d0d0a
Roman Artemev <roman.artemev@syntacore.com>
2026-03-09 13:33:08 +0300

    hyprland: fix hypr* configs after update

--- hypr/hyprpaper_realoder.sh
+++ hypr/hyprpaper_realoder.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 WALLPAPER_DIR="$HOME/Pictures/walls/"
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
-# Get a random wallpaper that is not the current one
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+WALLPAPER="$(find "$WALLPAPER_DIR" -type f | shuf -n 1)"
+MONITOR="$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')"
 
-# Apply the selected wallpaper
-hyprctl hyprpaper reload ,"$WALLPAPER"
+hyprctl hyprpaper wallpaper "$MONITOR, $WALLPAPER, cover"
